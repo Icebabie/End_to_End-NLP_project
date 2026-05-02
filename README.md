0cfbe1496fc27adfa6f244b4f02d94ca0d4fdec0
Divyansh Babel <babeldivyansh28@gmail.com>
2026-05-02 21:59:14 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # End_to_End-NLP_project
+
 A Hate speech classifier using NLP techniques to classify if comments are neutral or hateful.
+
+## How to run?
+
+'''bash
+conda create -n venv python==3.12 -y
+'''
+'''bash
+conda activate venv
+'''
+'''bash
+pip install -r requirements.txt
+'''
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+  name = "Hate_speech_classifier",
+  version = "0.0.1",
+  author = "Icebabie",
+  author_email = "babeldivyansh28@gmail.com",
+  description = "A hate speech classifier project",
+  packages = find_packages(),
+  install_requires = []
+)
