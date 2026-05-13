870270b8e7316f280ddf412598555c94eee8b6c7
Divyansh Babel <babeldivyansh28@gmail.com>
2026-05-14 01:32:24 +0530

    gcloud_config added

---
+++ demo.py
@@ -0,0 +1,13 @@
+from Hate_speech_classifier.configuration.gcloud_syncer import GcloudSync
+from Hate_speech_classifier.logger import logging
+from Hate_speech_classifier.exception import CustomException
+import sys
+
+# logging.info("This is a demo log message from demo.py to check if the logging is working properly.")
+# try:
+#   a = 7/"0"
+# except Exception as e:
+#   raise CustomException(e, sys)
+# tested gcloud syncer
+# obj = GcloudSync()
+# obj.sync_folder_from_gcloud("hate_speech_icebabie","dataset.zip","downloads/Dataset.zip")
